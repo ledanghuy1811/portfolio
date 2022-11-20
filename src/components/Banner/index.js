90f82f74a75aa3d9045a5ecb7746d13b724262cd
ledanghuy1811 <huygamo567@gmail.com>
2022-11-21 00:15:31 +0700

    done UI banner

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,14 +1,44 @@
-import React from 'react'
-import classNames from 'classnames/bind'
+import React from 'react';
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
-import styles from './Banner.module.scss'
+import styles from './Banner.module.scss';
+import bannerImage from '~/assets/img/header-img.svg';
 
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
 
 const Banner = () => {
   return (
-    <div className={cx('wrapper')} style={{ height: 1000}}></div>
-  )
-}
+    <div className={cx('wrapper')}>
+      <div className={cx('container', 'grid grid-cols-1 sm:grid-cols-12')}>
+        <div className={cx('info', 'sm:col-span-7')}>
+          <h3 className={cx('welcome')}>Welcome to my Portfolio</h3>
+          <h1 className={cx('name')}>Hi! I'm Huy Web Developer</h1>
+          <p className={cx('description')}>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+          <a href="#contact" className={cx('connect')}>
+            <p className={cx('connect-text')}>Let's Connect</p>
+            <spam className={cx('connect-icon')}>
+              <FontAwesomeIcon icon={faArrowRight} />
+            </spam>
+          </a>
+        </div>
 
-export default Banner
+        <div className={cx('image', 'sm:col-span-5')}>
+          <img
+            src={bannerImage}
+            alt="Banner"
+            className={cx('banner-img')}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
